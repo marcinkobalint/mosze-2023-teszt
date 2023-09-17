38d71039c2e46889b716c57758f75e4099d4434d
marcinkobalint <71631313+marcinkobalint@users.noreply.github.com>
2023-09-17 20:31:27 +0200

    hibák voltak a kódban

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 100; 
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; hiba#1
+    std::cout << '1-100 ertekek duplazasa' hiba#2
+    for (int i = 0;) hiba#3
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) hiba#4
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; hiba#5
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] hiba#6
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    hiba#7
     return 0;
 }
