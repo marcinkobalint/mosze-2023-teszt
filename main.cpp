c3bb0e36572876f407aadf4a33e0715c546601aa
marcinkobalint <71631313+marcinkobalint@users.noreply.github.com>
2023-09-17 20:40:22 +0200

    hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; hiba#1
-    std::cout << '1-100 ertekek duplazasa' hiba#2
-    for (int i = 0;) hiba#3
+    int *b = new int[N_ELEMENTS]; javítva #1
+    std::cout << '1-100 ertekek duplazasa' << std::endl; javítva #2
+    for (int i = 0; i < N_ELEMENTS; i++) javítva#3
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) hiba#4
+    for (int i = 0; i < N_ELEMENTS; i++) javítva#4
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; hiba#5
+    int atlag = 0; javítva#5
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i] hiba#6
+        atlag += b[i]; javítva#6
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    hiba#7
+    delete[] b;  javítva#7
     return 0;
 }
