82c883abe449a2b6aaa50a391fab38f5e1d414ea
benedek461 <snuppdawg@gmail.com>
2023-09-21 23:53:10 +0200

    Added extra cout.

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[N_ELEMENTS]; javítva #1
-    std::cout << '1-100 ertekek duplazasa' << std::endl; javítva #2
-    for (int i = 0; i < N_ELEMENTS; i++) javítva#3
+    int *b = new int[N_ELEMENTS]; //javítva #1
+    std::cout << '1-100 ertekek duplazasa' << std::endl; //javítva #2
+    for (int i = 0; i < N_ELEMENTS; i++) //javítva#3
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i < N_ELEMENTS; i++) javítva#4
+    for (int i = 0; i < N_ELEMENTS; i++) //javítva#4
     {
         std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag = 0; javítva#5
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0; //javítva#5
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]; javítva#6
+        atlag += b[i]; //javítva#6
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    delete[] b;  javítva#7
+    delete[] b;  //javítva#7
+    std::cout << "Mi az piros és roncsolja a fogat? Tégla." << std::endl;
     return 0;
 }
